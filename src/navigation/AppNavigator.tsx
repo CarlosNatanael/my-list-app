0862d23f24d227527e0fbfcd3d6d313d282b33c0
Srleo12 <carlosnatha2345@gmail.com>
2025-07-22 13:26:25 -0300

    .

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -3,11 +3,20 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { View, Text } from 'react-native';
 
-// Tela de Home Temporária para teste
+// Tela de Home Temporária
 function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Tela Inicial Funciona!</Text>
+      <Text>Tela Inicial</Text>
+    </View>
+  );
+}
+
+// Tela de Carrinho Temporária
+function CartScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Tela do Carrinho</Text>
     </View>
   );
 }
@@ -17,8 +26,9 @@ const Stack = createNativeStackNavigator();
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Cart" component={CartScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
