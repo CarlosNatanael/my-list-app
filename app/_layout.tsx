8915d87eee842609ea0a4b40accdee437b588204
Srleo12 <carlosnatha2345@gmail.com>
2025-08-06 15:34:05 -0300

    .

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { ListProvider } from '../../context/ListContext'; // Corrija o caminho se você moveu o arquivo
+import { ListProvider } from '../context/ListContext';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Stack, SplashScreen } from 'expo-router';
 
--- app/list.tsx
+++ app/list.tsx
@@ -3,11 +3,11 @@ import { SectionList, StatusBar, View, Text, TouchableOpacity, StyleSheet, Share
 import { Link, Stack } from 'expo-router';
 import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Plus, ShoppingCart, DollarSign, Pencil, Menu, Share2, Check } from 'lucide-react-native';
-import { useList, Item } from '../../context/ListContext';
-import { AddItemForm } from '../../components/AddItemForm';
-import { PriceModal } from '../../components/PriceModal';
-import { EditItemModal } from '../../components/EditItemModal';
-import { MenuModal } from '../../components/MenuModal';
+import { useList, Item } from '../context/ListContext';
+import { AddItemForm } from '../components/AddItemForm';
+import { PriceModal } from '../components/PriceModal';
+import { EditItemModal } from '../components/EditItemModal';
+import { MenuModal } from '../components/MenuModal';
 
 export default function ListScreen() {
   const { 
