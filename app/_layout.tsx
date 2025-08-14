20f8499425aeb863f310abd7cfb794f80abdc25f
Srleo12 <carlosnatha2345@gmail.com>
2025-08-14 10:27:03 -0300

    .

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { ListProvider } from '../context/ListContext';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Stack, SplashScreen } from 'expo-router';
-import { useEffect } from 'react'; // 1. Importe o useEffect
+import { useEffect } from 'react';
 
 // Impede que a tela de splash suma automaticamente
 SplashScreen.preventAutoHideAsync();
